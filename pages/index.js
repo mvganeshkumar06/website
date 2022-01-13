145c52bded9448290e352f7ab764df1b1380a18d
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-01-13 12:53:00 +0530

    updated page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Text } from '@zeal-ui/core';
+import { Layout, Text, Container } from '@zeal-ui/core';
 import { HomeStyled, SkillStyled } from '../styles/index';
 import { educationItems, experienceItems, skillItems, projectItems } from '../util/index';
 import { Education, Experience, Skill, Project } from '../components/index';
@@ -8,59 +8,63 @@ const Home = () => {
 	return (
 		<Layout title="Home">
 			<HomeStyled type="col" width="100%" rowCenter>
-				<Text type="mainHeading" width="100%" center>
-					Home
-				</Text>
-				<Text type="subHeading1" className="homeSubHeading">
-					Education
-				</Text>
-				{educationItems.map(({ title, duration, qualification, grade }) => {
-					return (
-						<Education
-							key={title}
-							title={title}
-							duration={duration}
-							qualification={qualification}
-							grade={grade}
-						/>
-					);
-				})}
-				<Text type="subHeading1" className="homeSubHeading">
-					Experience
-				</Text>
-				{experienceItems.map(({ title, duration, details }) => {
-					return (
-						<Experience
-							key={title}
-							title={title}
-							duration={duration}
-							details={details}
-						/>
-					);
-				})}
-				<Text type="subHeading1" className="homeSubHeading">
-					Skills
-				</Text>
-				<SkillStyled type="col" width="100%" className="skillContainer">
-					{skillItems.map(({ title, values }) => {
-						return <Skill key={title} title={title} values={values} />;
+				<Container type="col" width="100%" className="homeContainer">
+					<Text>
+						Hi there ! I'm M V Ganesh Kumar, a passionate full stack developer with
+						strong problem solving skills. Welcome to my space on the web where I share
+						my learnings, experiences and showcase my work.
+					</Text>
+					<Text type="subHeading1" className="homeSubHeading">
+						🏫 Education
+					</Text>
+					{educationItems.map(({ title, duration, qualification, grade }) => {
+						return (
+							<Education
+								key={title}
+								title={title}
+								duration={duration}
+								qualification={qualification}
+								grade={grade}
+							/>
+						);
 					})}
-				</SkillStyled>
-				<Text type="subHeading1" className="homeSubHeading">
-					Projects
-				</Text>
-				{projectItems.map(({ title, tags, details, source, live }) => {
-					return (
-						<Project
-							key={title}
-							title={title}
-							tags={tags}
-							details={details}
-							source={source}
-							live={live}
-						/>
-					);
-				})}
+					<Text type="subHeading1" className="homeSubHeading">
+						🏢 Experience
+					</Text>
+					{experienceItems.map(({ title, duration, details }) => {
+						return (
+							<Experience
+								key={title}
+								title={title}
+								duration={duration}
+								details={details}
+							/>
+						);
+					})}
+					<Text type="subHeading1" className="homeSubHeading">
+						⚙️ Skills
+					</Text>
+					<SkillStyled type="col" width="100%" className="skillContainer">
+						{skillItems.map(({ title, values }) => {
+							return <Skill key={title} title={title} values={values} />;
+						})}
+					</SkillStyled>
+					<Text type="subHeading1" className="homeSubHeading">
+						⚒️ Projects
+					</Text>
+					{projectItems.map(({ title, tags, details, source, live }) => {
+						return (
+							<Project
+								key={title}
+								title={title}
+								tags={tags}
+								details={details}
+								source={source}
+								live={live}
+							/>
+						);
+					})}
+				</Container>
 			</HomeStyled>
 		</Layout>
 	);
