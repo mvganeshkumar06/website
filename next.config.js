8aad55f5419d5ebdfbb74748836ccf4174c2b72e
M V Ganesh Kumar <contact@mvganeshkumar.com>
2022-02-06 21:58:06 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const config = {
 	reactStrictMode: true,
+	pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+	webpack(config, options) {
+		config.module.rules.push({
+			test: /\.mdx?$/,
+			use: [
+				options.defaultLoaders.babel,
+				{
+					loader: '@mdx-js/loader',
+				},
+			],
+		});
+		return config;
+	},
 };
+
+module.exports = config;
